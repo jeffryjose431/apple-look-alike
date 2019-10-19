9990b4b92e3fc640c10750c543d5ada4226ad5f6
Jeffry Jose <jeffryjose431@live.com>
2019-10-19 00:15:38 -0400

    Second part of footer

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,8 @@ const selectElement = element => document.querySelector(element);
 selectElement(".mobile-menu").addEventListener("click", () => {
   selectElement("header").classList.toggle("active");
 });
+
+// selectElement(".main-title").addEventListener("click", () => {
+//   selectElement(".main-title").classList.toggle("active");
+// });
+
