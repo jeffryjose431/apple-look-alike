29b0b525f1bba7dfef593c1a15db781f02dfd181
Jeffry Jose <jeffryjose431@live.com>
2019-10-19 19:31:31 -0400

    Finish

--- javascript/script.js
+++ javascript/script.js
@@ -3,8 +3,3 @@ const selectElement = element => document.querySelector(element);
 selectElement(".mobile-menu").addEventListener("click", () => {
   selectElement("header").classList.toggle("active");
 });
-
-// selectElement(".main-title").addEventListener("click", () => {
-//   selectElement(".main-title").classList.toggle("active");
-// });
-
